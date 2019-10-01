b0063777c1cc07a9f0df1ffd134bffa969de4538
溝口正樹 <>
2019-10-01 15:18:33 +0900

    プロジェクトの作成

---
+++ generalSys/src/main/java/com/general/demo/GeneralSysApplication.java
@@ -0,0 +1,13 @@
+package com.general.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneralSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneralSysApplication.class, args);
+	}
+
+}
